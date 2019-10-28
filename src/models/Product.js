41fdc4aaf0cf0771b53409e6f3be169fd386474e
Pedro Moura <pedro.moura@imusica.com.br>
2019-10-28 17:07:07 -0300

    cors

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+const mongoosePaginate = require('mongoose-paginate');
+
 const ProductSchema = new mongoose.Schema({
     title:{
         type: String,
@@ -19,5 +21,7 @@ const ProductSchema = new mongoose.Schema({
     }
 
 });
+
+ProductSchema.plugin(mongoosePaginate);
 
 mongoose.model('Product',ProductSchema);
